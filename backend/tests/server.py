cf4d524db8aa32c78eaa4771e77bc763b68e0b4f
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2019-12-14 20:09:32 +0530

    #5: Added start and stop methods to launch the WSGI server and handle resources.

--- backend/tests/server.py
+++ backend/tests/server.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 from threading import Thread
@@ -7,7 +9,10 @@ class BackendServer:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
+
+        # create the flask app and the wsgi server
         self._app = self._create_flask_app()
+        self._wsgi_server = None  # has to be set inside the _run_server function
         self._thread = Thread(target=self._run_server)
 
     @staticmethod
@@ -25,10 +30,39 @@ class BackendServer:
         return app
 
     def _run_server(self):
-        pass
+        """
+        Helper function to start the wsgi server in a separate thread, using the
+        pointers to the instantiated WSGI server from the constructor.
+        """
+        self._wsgi_server = WSGIServer((self.host, self.port), self._app)
+        self._wsgi_server.serve_forever()
 
     def start(self):
-        pass
+        if not self._thread.is_alive():
+            self._thread.start()
+            print(f"Started BackendServer on {self.host}:{self.port}")
+        else:
+            print("The BackendServer is already running")
 
-    def stop(self):
-        pass
+    def stop(self, timeout=2):
+        if self._thread.is_alive() and self._wsgi_server is not None:
+            # kill the WSGI server and then kill the thread
+            self._wsgi_server.stop(timeout=timeout)
+            self._wsgi_server.close()
+            self._thread.join(timeout=timeout + 2)
+
+            # sleep to give the server time to kill requests
+            time.sleep(1)
+            if self._thread.is_alive():
+                print("Error: Server Thread did not shutdown cleanly")
+            else:
+                print("BackendServer successfully shutdown")
+        else:
+            print("The BackendServer is already shutdown")
+
+
+if __name__ == "__main__":
+    server = BackendServer("localhost", 5000)
+    server.start()
+    _ = input("Hit enter to kill ... ")
+    server.stop()
