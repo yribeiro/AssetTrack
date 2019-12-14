445655fc750b0f09e12db745387295d788fc3c94
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2019-12-14 23:21:57 +0530

    #2: Added a single test to check the endpoint.

--- backend/tests/test_server.py
+++ backend/tests/test_server.py
@@ -43,3 +43,34 @@ class TestBackendServer(unittest.TestCase):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((self.host, self.port))  # Try to open port - passes if connection is made
         s.close()
+
+    def test_server_add_user_endpoint(self):
+        server = BackendServer(host=self.host, port=self.port)
+        server.start()
+        time.sleep(1)  # sleep to allow server to start
+
+        # make a request on the add user end point as get
+        resp = requests.get(f"http://{self.host}:{self.port}/api/add_user")
+        self.assertEqual(resp.status_code, 405)
+
+        # make valid request as post with user data
+        resp = requests.post(
+            f"http://{self.host}:{self.port}/api/add_user",
+            json={
+                "firstname": "Yohahn", "lastname": "Ribeiro",
+                "age": 25, "email": "  yohahnribeiro29@gmail.com"
+            }
+        )
+        self.assertEqual(resp.status_code, 200)
+
+        # make valid request with same email address
+        resp = requests.post(
+            f"http://{self.host}:{self.port}/api/add_user",
+            json={
+                "firstname": "Yohahn", "lastname": "Ribeiro",
+                "age": 25, "email": "  yohahnribeiro29@gmail.com"
+            }
+        )
+        self.assertEqual(500, resp.status_code)
+
+        server.stop()
