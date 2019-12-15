9ff12bbc1e1ae153892dbded597f4a9aea997819
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2019-12-15 11:32:07 +0530

    #2: Added persist to disk functionality in main method, and changed datastore to use the right variables.

--- backend/datastore.py
+++ backend/datastore.py
@@ -15,25 +15,34 @@ class InMemoryDataStore:
 
     _USERS_LOCK: Lock = Lock()
 
-    def __init__(self):
-        pass
+    def __init__(self, users_pck_file: str = None):
+        """
+        Constructor
 
-    def add_user(self, firstname: str, lastname: str, age: int, email: str):
-        with self._USERS_LOCK:
-            if list(filter(lambda user: user.email == email, self._USERS)):
+        :param users_pck_file: .pck file that contains a stored user list.
+        """
+        if users_pck_file is not None:
+            with open(users_pck_file, "rb") as fp:
+                InMemoryDataStore._USERS = pickle.load(fp)
+
+    @staticmethod
+    def add_user(firstname: str, lastname: str, age: int, email: str):
+        with InMemoryDataStore._USERS_LOCK:
+            if list(filter(lambda user: user.email == email, InMemoryDataStore._USERS)):
                 raise ValueError("This email address has already been registered")
             else:
                 u = User(firstname, lastname, age, email)
-                self._USERS.append(u)
+                InMemoryDataStore._USERS.append(u)
                 print(f"Added new user {u.first_name} {u.last_name}", file=sys.stdout)
 
-    def save_to_disk(self):
+    @staticmethod
+    def save_to_disk():
         """
         Function to persist the data storage to disk. Stored in the backend storage
         area.
         """
         file_path = os.path.join(STORAGE_PATH, "users.pck")
         with open(file_path, "wb") as fp:
-            pickle.dump(self._USERS, fp)
+            pickle.dump(InMemoryDataStore._USERS, fp)
 
         print(f"Saved all users to file: {file_path}")
--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,15 @@ if __name__ == "__main__":
     if not os.path.isdir(STORAGE_PATH):
         os.makedirs(STORAGE_PATH)
 
-    if os.listdir(STORAGE_PATH):
+    if "users.pck" in os.listdir(STORAGE_PATH):
         # load data into memory
-        pass
+        InMemoryDataStore(users_pck_file=os.path.join(STORAGE_PATH, "users.pck"))
 
     # start the servers
+    server = BackendServer("localhost", 5000)
+    server.start()
+    _ = input("Hit enter to kill ... ")
+    server.stop()
+
+    # persist the data to disk
+    InMemoryDataStore().save_to_disk()
