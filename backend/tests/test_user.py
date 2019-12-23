4b97d17825164bae84a59a35a37692b6df6c5aa1
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2019-12-23 10:48:35 +0530

    #6: Added test for the user json function.

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -2,7 +2,10 @@ import os
 import pickle
 import unittest
 
-import utils
+try:
+    from . import utils
+except ImportError:
+    import utils
 
 from backend.models import User
 
@@ -38,3 +41,14 @@ class TestUserClass(unittest.TestCase):
         self.assertEqual(u.last_name, u2.last_name)
         self.assertEqual(u.age, u2.age)
         self.assertEqual(u.email, u2.email)
+
+    def test_user_json_dict_is_generated_correctly(self):
+        u = utils.get_test_user()
+
+        ujson = u.to_json()
+
+        # check with camecase keys
+        self.assertEqual(ujson["firstName"], u.first_name)
+        self.assertEqual(ujson["lastName"], u.last_name)
+        self.assertEqual(ujson["age"], u.age)
+        self.assertEqual(ujson["email"], u.email)
