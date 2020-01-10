4c2df2ce733a833040993b189c6f6974ad58a4ba
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2020-01-10 20:53:29 +0000

    #13: Added divs and buttons for the update and net worth display.

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="dashboard">
     <b-container>
-        <h3>{{ msg }}</h3>
+      <h3>{{ msg }}</h3>
     </b-container>
     <b-container class="mt-3">
-        <b-row class="text-center">
-            <b-col cols="8" class="border border-primary mr-2"> Test Chart Area </b-col>
-            <b-col class="border border-primary"> Test Button and Net Worth Area </b-col>
-        </b-row>
+      <b-row class="text-center">
+        <b-col cols="8" class="border border-primary mr-2">Test Chart Area</b-col>
+        <b-col class="border border-primary">
+          <b-button size="lg" variant="info" id="updateButton">Update</b-button>
+          <div class="border rounded border-secondary mt-3" id="netWorth">
+              <p class="mt-1"><b>Net Worth</b></p>
+              <h2 class="mt-1 mb-2">£32,000</h2>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
     <b-container class="mt-3">
-        <b-row class="text-center">
-            <b-col class="border border-primary mr-2"> Pie Chart Area 1 </b-col>
-            <b-col class="border border-primary"> Pie Chart Area 2 </b-col>
-        </b-row>
+      <b-row class="text-center">
+        <b-col class="border border-primary mr-2">Pie Chart Area 1</b-col>
+        <b-col class="border border-primary">Pie Chart Area 2</b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -42,5 +48,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+#updateButton {
+    width: inherit;
 }
 </style>
